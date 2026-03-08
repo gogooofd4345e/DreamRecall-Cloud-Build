dd12e465d7000788fa8a32c5bea84625143c9475
MUSTTTTAFA <mustmuhyie@gmail.com>
2026-03-09 01:01:50 +0300

    Add Material Components dependency to fix theme error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.android.material:material:1.11.0")
 }
